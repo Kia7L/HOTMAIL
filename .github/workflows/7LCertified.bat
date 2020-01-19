421f39d0bb324b87cc49fb15a54472f1a112512c
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-01-18 15:01:28 -1000

    exe included

--- .github/workflows/7LCertified.bat
+++
@@ -1,7 +0,0 @@
-cd \
-@echo off
-@color 04
-Mode con: cols=60 lines=6
-start chrome.exe "https://github.com/Kia7L/HOTMAIL/blob/master/7L-Inentory.html"
-"7LCertiffied.yml"
-exit
---
+++ WEBSITE By Kia7L.py
@@ -0,0 +1,23 @@
+import os
+import colorama
+import webbrowser
+from os import system
+from time import sleep
+from colorama import *
+
+#-Keep-Edit------------------------------
+TitleZero=('title Hello7L')
+os.system(TitleZero)
+os.system("color 06")
+os.system("mode con: cols=55 lines=11")
+cls = 'cls'
+os.system(cls)
+#-Keep-Edit------------------------------
+
+#-Code--------------------------
+print ("html")
+html=('.\\7L-Inentory.html')
+html2=('.\\*.html')
+os.system(html)
+os.system(html2)
+#-Code--------------------------

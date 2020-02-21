2b4afc8490bb80c39860c001ecd38a8cdb370f8e
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-02-20 14:56:29 -1000

    fix

---
+++ Kia7LWebpage.py
@@ -0,0 +1,18 @@
+import webbrowser
+import os
+from os import *
+from time import sleep
+Title=('title 7L.INVENTORY')
+os.system('Title')
+os.system("color 01")
+os.system("mode 40,5")
+cls = 'cls'
+os.system(cls)
+
+print("By Kia7L: PYTHON MADE")
+start='start '
+WebViewer='"https://htmlpreview.github.io/?'
+WebPage='https://github.com/Kia7L/HOTMAIL/Pictures/blob/master/Kia7L.html"'
+webbrowser.open_new(WebViewer+WebPage)
+sleep(7)
+
